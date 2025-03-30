bab3aed10cfb522fe6d7deba9f393b553eb84798
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2025-03-30 02:29:09 +0200

    update columns

--- src/features/products/columns.tsx
+++ src/features/products/columns.tsx
@@ -2,7 +2,7 @@ import { ColumnDef, Row } from "@tanstack/react-table";
 import { Product } from "./type";
 import { Checkbox } from "../../components/ui/checkbox";
 import { Button } from "../../components/ui/button";
-import { SquarePen, Trash2 } from "lucide-react";
+import {  Trash2 } from "lucide-react";
 import { useTranslation } from "react-i18next";
 
 export const getProductColumns = (isRTL: boolean): ColumnDef<Product>[] => {
@@ -152,16 +152,10 @@ export const getProductColumns = (isRTL: boolean): ColumnDef<Product>[] => {
       enableHiding: false,
       cell: ({ row, table }: { row: Row<Product>; table: any }) => {
         const onDelete = table.options.meta?.onDelete;
-        const onEdit = table.options.meta?.onEdit;
         
         return (
           <div className="flex items-center gap-2 justify-center text-primary">
-            <SquarePen
-              size={24}
-              className="cursor-pointer hover:text-blue-500"
-              onClick={() => onEdit && onEdit(String(row.original))}
-              aria-label={t("common.edit")}
-            />
+           
             
             <Trash2
               size={24}
